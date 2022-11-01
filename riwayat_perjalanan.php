3bca2c408f77ce3093e2e70e745fdd5c67f47bb4
awlfdl09 <110008807+awlfdl09@users.noreply.github.com>
2022-11-01 11:53:47 +0700

    Add files via upload

---
+++ riwayat_perjalanan.php
@@ -0,0 +1,29 @@
+<div class="card">
+     <div class="card-header">
+        <h3 class="card-title">Riwayat Perjalanan</h3>
+    </div>
+        <!-- /.card-header -->
+         <div class="card-body">
+         <table id="example1" class="table table-bordered table-striped">
+<thead>
+<tr>
+    <th>No</th>
+    <th>Tanggal</th>
+    <th>Jam</th>
+    <th>Lokasi Berkunjung</th>
+    <th>Suhu Tubuh</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+</tr>
+</table>
+</div>
+<!-- /.card-body -->
+</div>
+<!-- /.card -->
---
+++ tulisan_catatan.php
@@ -0,0 +1,41 @@
+<div class="card card-info">
+<div class="card-header">
+<h3 class="card-title">Tulis Catatan</h3>
+</div>
+
+
+<form action=""method="post">
+<div class="card-body">
+<div class="form-group row">
+<label class="col-sm-2 col-form-label">Pilih Tanggal</label>
+<div class="col-sm-10">
+<input type="date" name="tanggal" class="form-control">
+</div>
+</div>
+<div class="form-group row">
+<label class="col-sm-2 col-form-label">Pilih Jam</label>
+<div class="col-sm-10">
+<input type="time" name="jam" class="form-control">
+</div>
+</div>
+<div class="form-group row">
+<label class="col-sm-2 col-form-label">Lokasi</label>
+<div class="col-sm-10">
+<input type="text" name="lokasi" class="form-control" placeholder="Masukkan Lokasi">
+</div>
+</div>
+<div class="form-group row">
+<label class="col-sm-2 col-form-label">Suhu Tubuh</label>
+<div class="col-sm-10">
+<input type="number" name="suhu" class="form-control" placeholder="Masukkan Suhu Tubuh(celcius)">
+</div>
+</div>
+</div>
+
+<div class="card-footer">
+<button type="submit" class="btn btn-success float-right m-2"><i class="fa fa-save"></i> simpan</button>
+<button type="reset" class="btn btn-warning float-right m-2"><i class="fa fa-trash"></i> kosongkan</button>
+</div>
+
+</form>
+</div>
